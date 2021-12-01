7573f8f4a214604dbe684514be2bb52aec39941a
Karen Sittig <kasittig@Karens-MacBook-Air.local>
2021-12-01 15:42:41 -0500

    Solution for 2021 day 1

--- daily_solutions/__init__.py
+++ daily_solutions/__init__.py
@@ -1,11 +1,10 @@
-from typing import Callable
-
-from frozendict import frozendict
+from typing import Callable, Dict
 
 from daily_solutions.year_2020 import DAYS_TO_SOLUTIONS as solutions_2020
 from daily_solutions.year_2021 import DAYS_TO_SOLUTIONS as solutions_2021
 
-SOLUTIONS_BY_YEAR: frozendict[str, frozendict[str, Callable]] = frozendict(
-    {"2021": solutions_2021, "2020": solutions_2020}
-)
+SOLUTIONS_BY_YEAR: Dict[str, Dict[str, Callable]] = {
+    "2021": solutions_2021,
+    "2020": solutions_2020,
+}
 DEFAULT_YEAR = "2021"
--- daily_solutions/year_2021/__init__.py
+++ daily_solutions/year_2021/__init__.py
@@ -1,6 +1,5 @@
-from typing import Callable
+from typing import Callable, Dict
 
 from daily_solutions.year_2021.day_1 import solve_day_1
-from frozendict import frozendict
 
-DAYS_TO_SOLUTIONS: frozendict[str, Callable] = frozendict({"1": solve_day_1})
+DAYS_TO_SOLUTIONS: Dict[str, Callable] = {"1": solve_day_1}
