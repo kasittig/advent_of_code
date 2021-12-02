cfe18631fdf5b80b5ee183ed5d43e715f0e4c6a3
Karen Sittig <kasittig@Karens-MacBook-Air.local>
2021-12-02 00:17:51 -0500

    2021 Day 2 + boilerplate for 2020 Day 9

--- daily_solutions/year_2021/__init__.py
+++ daily_solutions/year_2021/__init__.py
@@ -1,5 +1,9 @@
 from typing import Callable, Dict
 
 from daily_solutions.year_2021.day_1 import Year2021Day1Solution
+from daily_solutions.year_2021.day_2 import Year2021Day2Solution
 
-DAYS_TO_SOLUTIONS: Dict[str, Callable] = {"1": Year2021Day1Solution}
+DAYS_TO_SOLUTIONS: Dict[str, Callable] = {
+    "1": Year2021Day1Solution,
+    "2": Year2021Day2Solution,
+}
