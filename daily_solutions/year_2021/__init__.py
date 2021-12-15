80e7baecf9ed6ed8b194cd53d7ea506836fcbcaa
Karen Sittig <kasittig@gmail.com>
2021-12-15 11:08:55 -0500

    2021 day 3 part 1

--- daily_solutions/year_2021/__init__.py
+++ daily_solutions/year_2021/__init__.py
@@ -2,8 +2,10 @@ from typing import Callable, Dict
 
 from daily_solutions.year_2021.day_1 import Year2021Day1Solution
 from daily_solutions.year_2021.day_2 import Year2021Day2Solution
+from daily_solutions.year_2021.day_3 import Year2021Day3Solution
 
 DAYS_TO_SOLUTIONS: Dict[str, Callable] = {
     "1": Year2021Day1Solution,
     "2": Year2021Day2Solution,
+    "3": Year2021Day3Solution,
 }
